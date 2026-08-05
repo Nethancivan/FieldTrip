910c35ea5a1ba85264746863bd8380c682c025f4
Nethancivan <thienhongvan48@gmail.com>
2026-08-05 22:31:44 +0700

    Update fieldtrips.js

--- src/constants/fieldtrips.js
+++ src/constants/fieldtrips.js
@@ -11,4 +11,8 @@ export const fieldtrips = [
     name: "PL02_FieldTrip2 Nghệ An",
     code: "06.PL02-FT-Nghean",
   },
+   {
+    name: "Series Trà Vinh",
+    code: "07. SR-FT-Travinh",
+  },
 ];
