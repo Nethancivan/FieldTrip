3e449c4441e433f64d23476f6858458bbc094beb
Nethancivan <thienhongvan48@gmail.com>
2026-09-07 15:47:49 +0700

    Update fieldtrips.js

--- src/constants/fieldtrips.js
+++ src/constants/fieldtrips.js
@@ -1,18 +1,34 @@
 export const fieldtrips = [
   {
     name: "PL01_FieldTrip Miền Trung",
-    code: "04.PL01-FT-Mientrung",
+    code: "01.PL01-FT-Mientrung",
   },
   {
     name: "ShootingMV Phú Thọ",
     code: "05.ST-MV-PT",
   },
+   {
+    name: "04. GVN_Fieldtrip_TraVinh",
+    code: "04. GVN_Fieldtrip_TraVinh",
+  },
     {
-    name: "PL02_FieldTrip2 Nghệ An",
-    code: "06.PL02-FT-Nghean",
+    name: "07. PL-FT-NinhbinhHN",
+    code: "07. PL-FT-NinhbinhHN",
   },
    {
-    name: "Series Trà Vinh",
-    code: "07. SR-FT-Travinh",
+    name: "08. Phiên làm việc HĐCV",
+    code: "08. HDCV-T08",
+  },
+  {
+    name: "09. CHI PHÍ CỐ VẤN - GĐ1",
+    code: "09. CPCV-GĐ1",
+  },
+    {
+    name: "10. MOU-Series - Trà Vinh",
+    code: "10. MOU-Series-TV",
+  },
+    {
+    name: "11. Phiên làm việc HĐCV - T10",
+    code: "11. HDCV-T10",
   },
 ];
